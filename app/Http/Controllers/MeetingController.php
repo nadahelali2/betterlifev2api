2cd9078d1e80e74562c5544f2ff2b2d3412dc909
nadahelali2 <helalinada2@gmail.com>
2024-05-19 01:05:43 +0100

    hello

---
+++ app/Http/Controllers/MeetingController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Meeting;
+use Illuminate\Http\Request;
+
+class MeetingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $meetings=Meeting::all();
+        return response()->json($meetings);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $meeting=new Meeting;
+        $meeting->name=$request->name;
+        $meeting->email=$request->email;
+        $meeting->phone=$request->phone;
+        $meeting->date=$request->date;
+        $meeting->save();
+        return response()->json(["message"=>"succes"],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Meeting $meeting)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MeetingController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/',[MeetingController::class,"index"]);
+Route::get('/store',[MeetingController::class,"store"]);
